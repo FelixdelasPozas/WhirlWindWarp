f3376d8896c1b0c2cfc375afa05f2a621c74f9f3
Félix de las Pozas Álvarez <felixdelaspozas@gmail.com>
2025-04-17 18:42:11 +0200

    Unify resource files.

---
+++ resources.h
@@ -0,0 +1,11 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define DLG_CONFIGURE                           4000
+#define IDC_CHECKBOX1                           4002
+#define IDC_CHECKBOX2                           4003
+#define IDC_CHECKBOX3                           4004
+#define IDC_COMBO2                              4005
+#define IDC_COMBO1                              4006
+#define IDC_OK                                  4008
